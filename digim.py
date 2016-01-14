57764660ef14e6891033a5e7e95e92c2baf43398
root <root@vps14581.inmotionhosting.com>
2016-01-13 23:54:18 -0500

    remove fluff

--- digim.py
+++ digim.py
@@ -10,10 +10,7 @@ from dns.rdatatype import *
 
 lineoptions = {"nlines": 10, "width": 100, "ptr": None}
 
-#def parsevars(domain):
-#    domain=domain
 
-# @click.option('-r', '--record_type')
 def pullrecord(domain, record_type):
     resolver = dns.resolver.Resolver()
     answers = resolver.query(domain, record_type.upper())
@@ -22,8 +19,8 @@ def pullrecord(domain, record_type):
 
 def getrecords(domain):
     pullrecord(domain, "A")
-#    pullrecord(domain, "cname")
-    pullrecord(domain, "mx")
+    pullrecord(domain, "CNAME")
+    pullrecord(domain, "MX")
     pullrecord(domain, "NS")
 
 
@@ -63,51 +60,6 @@ def zonerecords(domain):
     for record in zone:
         print record
 
-#    print "Getting zone object for domain", domain
-#    zone_file = "/var/named/%s.db" % domain
-#
-#    try:
-#        zone = dns.zone.from_file(zone_file, domain)
-#        print "Zone origin:", zone.origin
-#        for name, node in zone.nodes.items():
-#            rdatasets = node.rdatasets
-#            print "\n**** BEGIN NODE ****"
-#            print "node name:", name
-#            for rdataset in rdatasets:
-#                print "--- BEGIN RDATASET ---"
-#                print "rdataset string representation:", rdataset
-#                print "rdataset rdclass:", rdataset.rdclass
-#                print "rdataset rdtype:", rdataset.rdtype
-#                print "rdataset ttl:", rdataset.ttl
-#                print "rdataset has following rdata:"
-#                for rdata in rdataset:
-#                    print "-- BEGIN RDATA --"
-#                    print "rdata string representation:", rdata
-#                    if rdataset.rdtype == SOA:
-#                        print "** SOA-specific rdata **"
-#                        print "expire:", rdata.expire
-#                        print "minimum:", rdata.minimum
-#                        print "mname:", rdata.mname
-#                        print "refresh:", rdata.refresh
-#                        print "retry:", rdata.retry
-#                        print "rname:", rdata.rname
-#                        print "serial:", rdata.serial
-#                    if rdataset.rdtype == MX:
-#                        print "** MX-specific rdata **"
-#                        print "exchange:", rdata.exchange
-#                        print "preference:", rdata.preference
-#                    if rdataset.rdtype == NS:
-#                        print "** NS-specific rdata **"
-#                        print "target:", rdata.target
-#                    if rdataset.rdtype == CNAME:
-#                        print "** CNAME-specific rdata **"
-#                        print "target:", rdata.target
-#                    if rdataset.rdtype == A:
-#                        print "** A-specific rdata **"
-#                        print "address:", rdata.address
-#    except DNSException, e:
-#        print e.__class__, e
-#
 
 @click.command()
 @click.option('-d', '--domain', type=str )
